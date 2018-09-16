c26de8c3b31c2dda3fc52b0fabcf7fd07e1a059c
Francesco Cottone <kesiev@gmail.com>
2018-09-16 08:28:34 +0200

    New Game: Almost Love

---
+++ assets/images/almostlove/makesheets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd exports
+montage wall-park.png wall-roller.png wall-wheel.png wall-cups.png wall-bus.png wall-cafe.png  -tile 6x1 -geometry +0+0 -background none ../../../../tapes/almostlove/wallpapers.png
+montage silhouette-girl.png  silhouette-teacher.png -tile 2x1 -geometry +0+0 -background none ../../../../tapes/almostlove/body.png
+montage dress-shirt.png dress-student.png dress-sporty.png -tile 3x1 -geometry +0+0 -background none ../../../../tapes/almostlove/dress.png
+montage eyes-normalopen.png eyes-normalclosing.png eyes-normalalmostclosed.png eyes-normalclosed.png eyes-normalangry.png eyes-normalwink.png eyes-normalsmile.png -tile 7x1 -geometry +0+0 -background none ../../../../tapes/almostlove/eyes.png
+montage hairback-long.png hairback-short.png hairback-pigtails.png -tile 3x1 -geometry +0+0 -background none ../../../../tapes/almostlove/hairback.png
+montage hairtop-long.png hairtop-lock.png hairtop-soft.png -tile 3x1 -geometry +0+0 -background none ../../../../tapes/almostlove/hairtop.png
+montage mouth-half.png mouth-o.png mouth-closed.png mouth-olarge.png mouth-D.png mouth-halfD.png mouth-closeddown.png -tile 8x1 -geometry +0+0 -background none ../../../../tapes/almostlove/mouth.png
+
